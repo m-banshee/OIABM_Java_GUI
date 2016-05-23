fa9eb27503f86582283df9e3a7a7865c4949606b
m-banshee <markbanchy@gmail.com>
2016-05-22 23:09:21 -0400

    Initial attempt at undo functionality. Works well, not completely vetted for bugs yet.

--- src/Board.java
+++ src/Board.java
@@ -26,6 +26,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.Timer;
+import javax.swing.event.MenuKeyEvent;
+import javax.swing.event.MenuKeyListener;
 
 public class Board implements ActionListener, ItemListener
 {
@@ -37,7 +39,7 @@ public class Board implements ActionListener, ItemListener
 
 	public static final Point DECK_POS = new Point(5, 5);
 
-	private static BlueMoon deck;
+	private static BlueMoon deck, prevMove = null;
 
 	// GUI COMPONENTS (top level)
 	private static final JFrame frame = new JFrame("Once In A Blue Moon");
@@ -64,27 +66,52 @@ public class Board implements ActionListener, ItemListener
             menu.add(menuItem);
             menuItem.addActionListener(this);
 
+            menuBar.add(menu);
+            menuItem = new JMenuItem("Undo Move",
+                    KeyEvent.VK_U);
+            menu.add(menuItem);
+            menuItem.addActionListener(this);
+            
             menu = new JMenu("Help");
             menuBar.add(menu);
 
             frame.setJMenuBar(menuBar);
             return menuBar;
         }
-            
-
-
-            @Override
-            public void actionPerformed(ActionEvent e) 
+        
+        @Override
+        public void actionPerformed(ActionEvent e) 
+        {
+            JMenuItem source = (JMenuItem)(e.getSource());
+            if(source.getText() == "New Game")
             {
                 playNewGame();
             }
-
-            @Override
-            public void itemStateChanged(ItemEvent e) 
+            else if(source.getText() == "Undo Move")
             {
-                //...Get information from the item event...
-                //...Display it in the text area...
+                undoLastMove();
             }
+        }
+
+        @Override
+        public void itemStateChanged(ItemEvent e) 
+        {
+            System.out.println("item state change");
+        }
+        
+        public void undoLastMove()
+        {
+            if(this.prevMove != null)
+            {
+                System.out.println("trying to undo");
+                deck.popPrevMove();
+
+                table.removeAll();
+                table.add(deck);
+
+                table.repaint();
+            }
+        }
         
 	// moves a card to abs location within a component
 	protected static Card moveCard(Card c, int x, int y)
@@ -104,7 +131,7 @@ public class Board implements ActionListener, ItemListener
            
                 MouseEvent startE, stopE;
                 boolean released = false;
-                private Timer timer = new Timer(300, new ActionListener() 
+                private Timer timer = new Timer(200, new ActionListener() 
                 {
                     @Override
                     public void actionPerformed(ActionEvent e) 
@@ -113,6 +140,7 @@ public class Board implements ActionListener, ItemListener
                         {
                             // timer has gone off, so treat as a single click
                             //System.out.println("single!");
+                            prevMove = deck;
                             deck.handleMousePress(startE.getPoint());
                             deck.handleMouseRelease(startE.getPoint());
                         }
@@ -127,10 +155,12 @@ public class Board implements ActionListener, ItemListener
 		{
                     startE = e;
                     released = false;
+                    
                     if(timer.isRunning())
                     {
                         timer.stop();
                         //System.out.println("double click!");
+                        prevMove = deck;
                         deck.handleDoubleClick(e.getPoint());
                     }
                     else
@@ -150,6 +180,7 @@ public class Board implements ActionListener, ItemListener
                         stopE = e;
                         if(deck.isDragMove(startE.getPoint(), e.getPoint()))
                         {
+                            prevMove = deck;
                             deck.handleMouseRelease(e.getPoint());
                         }
                     }
